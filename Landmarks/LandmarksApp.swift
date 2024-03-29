3de5559a32490f803e2b3bac653768d59f4a8664
Sebastian Stoelen <gewoonseba@gmail.com>
2024-03-29 07:17:06 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sebastian Stoelen on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
