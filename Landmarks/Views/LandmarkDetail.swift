60209f56001f0923de3510881bc19abc21395157
Sebastian Stoelen <gewoonseba@gmail.com>
2024-03-29 17:50:12 +0100

    End of building lists

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Sebastian Stoelen on 29/03/2024.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
