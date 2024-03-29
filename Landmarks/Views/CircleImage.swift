7262077117910320a2dfc02aa350b9075eb8746a
Sebastian Stoelen <gewoonseba@gmail.com>
2024-03-29 12:53:45 +0100

    Bulding lists, section 5

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Sebastian Stoelen on 29/03/2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Landmarks/Views/LandmarksList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksList.swift
+//  Landmarks
+//
+//  Created by Sebastian Stoelen on 29/03/2024.
+//
+
+import SwiftUI
+
+struct LandmarksList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarksList()
+}
