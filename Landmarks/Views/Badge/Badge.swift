0efbe3ff4cbfce96062cc0e3545c53c3668eec38
Sebastian Stoelen <gewoonseba@gmail.com>
2024-03-30 21:16:23 +0100

    End of drawing paths and shapes

---
+++ Landmarks/Views/Badge/Badge.swift
@@ -0,0 +1,18 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Sebastian Stoelen on 30/03/2024.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Badge()
+}
