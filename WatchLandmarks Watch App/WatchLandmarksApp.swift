847c2d38387c3d294c800a648179cf2fd7e52885
Sebastian Stoelen <gewoonseba@gmail.com>
2024-03-31 20:18:55 +0200

    Added PageView to CategoryHome

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Sebastian Stoelen on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
